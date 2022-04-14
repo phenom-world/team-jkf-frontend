218b040ec85c26664e12e34df6e233617d855a90
official <tijjken@gmail.com>
2022-04-14 01:04:27 +0100

    done

--- src/components/Downloads/Downloads.js
+++ src/components/Downloads/Downloads.js
@@ -3,12 +3,12 @@ import React from "react";
 const Downloads = () => {
   return (
     <div className="mt-5 w-100 d-flex justify-content-center align-items-center">
-      <h5>
+      <h6>
         {" "}
         <i className="fa-solid fa-ban text-danger"></i>
         {""}
         {""} No Downloads Available
-      </h5>
+      </h6>
     </div>
   );
 };
