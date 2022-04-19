abaae5d928d3445dd85073c2da99114241ea4d29
official <tijjken@gmail.com>
2022-04-19 04:05:26 +0100

    done

--- src/network/index.js
+++ src/network/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/tjkf",
+  baseURL: "https://team-jkf.herokuapp.com/tjkf",
 });
 
 API.interceptors.request.use((req) => {
