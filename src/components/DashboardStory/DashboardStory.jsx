c3d76ba4d307ff4234275257db48baca50a90950
t-official <tijjken@gmail.com>
2022-05-02 19:49:20 +0100

    done

--- src/components/DashboardStory/DashboardStory.jsx
+++ src/components/DashboardStory/DashboardStory.jsx
@@ -6,7 +6,7 @@ function DashboardStory({ onClick, showResults }) {
     <div className="mx-5 mt-3">
       <h5>Welcome to Team JKF Membership Platform, You can now check for news update.</h5>
       <br />
-      <p>News and Update blogpost where members can pass comments on post</p>
+      {/* <p>News and Update blogpost where members can pass comments on post</p> */}
       {/* <CardComp showResults={showResults} onClick={onClick} /> */}
     </div>
   );
